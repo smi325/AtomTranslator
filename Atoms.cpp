0d727756598dab77e00995d8503182c8be389030
Michael Sladkov <sabpp@mail.ru>
2020-06-03 00:58:57 +0300

    Stages 1-5 completed

--- Atoms.cpp
+++ Atoms.cpp
@@ -48,3 +48,98 @@ std::string LabelOperand::toString() const
 	std::string tmp = std::to_string(_labelID);
 	return tmp;
 }
+
+
+
+
+
+
+
+
+
+
+BinaryOpAtom::BinaryOpAtom(const std::string& name, std::shared_ptr<RValue> left, std::shared_ptr<RValue> right, std::shared_ptr<MemoryOperand> result)
+{
+	_name = name;
+	_left = left;
+	_right = right;
+	_result = result;
+}
+
+UnaryOpAtom::UnaryOpAtom(const std::string& name, std::shared_ptr<RValue> operand, std::shared_ptr<MemoryOperand> result)
+{
+	_name = name;
+	_operand = operand;
+	_result = result;
+}
+
+ConditionalJumpAtom::ConditionalJumpAtom(const std::string& condition, std::shared_ptr<RValue> left, std::shared_ptr<RValue> right, std::shared_ptr<LabelOperand> label)
+{
+	_condition = condition;
+	_left = left;
+	_right = right;
+	_label = label;
+}
+
+JumpAtom::JumpAtom(std::shared_ptr<LabelOperand> label)
+{
+	_label = label;
+}
+
+LabelAtom::LabelAtom(std::shared_ptr<LabelOperand> label)
+{
+	_label = label;
+}
+
+InAtom::InAtom(std::shared_ptr<MemoryOperand> result)
+{
+	_result = result;
+}
+
+OutAtom::OutAtom(std::shared_ptr<Operand> value)
+{
+	_value = value;
+}
+
+
+std::string BinaryOpAtom::toString() const
+{
+	std::string tmp = "(" + _name + ", " + _left->toString() + ", " + _right->toString() + ", " + _result->toString() + ")";
+	return tmp;
+}
+
+std::string UnaryOpAtom::toString() const
+{
+	std::string tmp = "(" + _name + ", " + _operand->toString() + ", " + ", " + _result->toString() + ")";
+	return tmp;
+}
+
+std::string ConditionalJumpAtom::toString() const
+{
+	std::string tmp = "(" + _condition + ", " + _left->toString() + ", " + _right->toString() + ", " + _label->toString() + ")";
+	return tmp;
+}
+
+std::string JumpAtom::toString() const
+{
+	std::string tmp = "(JMP,,, " + _label->toString() + ")";
+	return tmp;
+}
+
+std::string LabelAtom::toString() const
+{
+	std::string tmp = "(LBL,,, " + _label->toString() + ")";
+	return tmp;
+}
+
+std::string InAtom::toString() const
+{
+	std::string tmp = "(IN,,, " + _result->toString() + ")";
+	return tmp;
+}
+
+std::string OutAtom::toString() const
+{
+	std::string tmp = "(OUT,,, " + _value->toString() + ")";
+	return tmp;
+}
--- Atoms.h
+++ Atoms.h
@@ -20,7 +20,7 @@ public:
 	RValue() {};
 };
 
-class MemoryOperand : RValue
+class MemoryOperand : public RValue
 {
 public:
 	MemoryOperand(int index, const SymbolTable* SymbolTable);
@@ -30,7 +30,7 @@ protected:
 	const SymbolTable* _SymbolTable;
 };
 
-class NumberOperand : RValue
+class NumberOperand : public RValue
 {
 public:
 	NumberOperand(int index);
@@ -39,7 +39,7 @@ protected:
 	int _index;
 };
 
-class StringOperand : Operand
+class StringOperand : public Operand
 {
 public:
 	StringOperand(int index, const StringTable* StringTable);
@@ -49,11 +49,98 @@ private:
 	const StringTable* _StringTable;
 };
 
-class LabelOperand : Operand
+class LabelOperand : public Operand
 {
 public:
 	LabelOperand(int labelID);
 	std::string toString() const;
 private:
 	int _labelID;
+};
+
+
+
+
+
+
+
+
+
+
+class Atom
+{
+public:
+	Atom() {};
+	virtual std::string toString() const = 0;
+};
+
+class BinaryOpAtom : public Atom
+{
+public:
+	BinaryOpAtom(const std::string& name, std::shared_ptr<RValue> left, std::shared_ptr<RValue> right, std::shared_ptr<MemoryOperand> result);
+	std::string toString() const;
+private:
+	std::string _name;
+	std::shared_ptr<RValue> _left;
+	std::shared_ptr<RValue> _right;
+	std::shared_ptr<MemoryOperand> _result;
+};
+
+class UnaryOpAtom : public Atom
+{
+public:
+	UnaryOpAtom(const std::string& name, std::shared_ptr<RValue> operand, std::shared_ptr<MemoryOperand> result);
+	std::string toString() const;
+private:
+	std::string _name;
+	std::shared_ptr<RValue> _operand;
+	std::shared_ptr<MemoryOperand> _result;
+};
+
+class ConditionalJumpAtom : public Atom
+{
+public:
+	ConditionalJumpAtom(const std::string& condition, std::shared_ptr<RValue> left, std::shared_ptr<RValue> right, std::shared_ptr<LabelOperand> label);
+	std::string toString() const;
+private:
+	std::string _condition;
+	std::shared_ptr<RValue> _left;
+	std::shared_ptr<RValue> _right;
+	std::shared_ptr<LabelOperand> _label; /*!!!!!!!!!!!!!!!!*/
+};
+
+class JumpAtom : public Atom
+{
+public:
+	JumpAtom(std::shared_ptr<LabelOperand> label);
+	std::string toString() const;
+private:
+	std::shared_ptr<LabelOperand> _label;
+};
+
+class LabelAtom : public Atom
+{
+public:
+	LabelAtom(std::shared_ptr<LabelOperand> label);
+	std::string toString() const;
+private:
+	std::shared_ptr<LabelOperand> _label;
+};
+
+class InAtom : public Atom
+{
+public:
+	InAtom(std::shared_ptr<MemoryOperand> result);
+	std::string toString() const;
+private:
+	std::shared_ptr<MemoryOperand> _result;
+};
+
+class OutAtom : public Atom
+{
+public:
+	OutAtom(std::shared_ptr<Operand> value);
+	std::string toString() const;
+private:
+	std::shared_ptr<Operand> _value;
 };
