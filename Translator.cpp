9d8dc7b017441fb53bcc0c5a035ddc31caa0fa02
Michael Sladkov <sabpp@mail.ru>
2020-06-04 18:52:03 +0300

    Stages 1-7 completed (+E1() test)

--- Translator.cpp
+++ Translator.cpp
@@ -1,10 +1,6 @@
 #include "Translator.h"
 
-Translator::Translator(std::istream& stream)
-{
-	Scanner _scanner = Scanner(stream);
-	Token _currentLexem = _scanner.getNextToken();
-}
+Translator::Translator(std::istream& stream) : _scanner(stream) {};
 
 void Translator::printAtoms(std::ostream& stream)
 {
@@ -27,4 +23,48 @@ std::shared_ptr<LabelOperand> Translator::newLabel()
 {
 	_lbl_counter += 1;
 	return std::make_shared<LabelOperand>(LabelOperand(_lbl_counter));
+}
+
+void Translator::syntaxError(const std::string& message)
+{
+	try
+	{
+		throw 1;
+	}
+	catch (int a)
+	{
+		std::cout << message;
+	}
+}
+
+void Translator::lexicalError(const std::string& message)
+{
+	try
+	{
+		throw 1;
+	}
+	catch (int a)
+	{
+		std::cout << message;
+	}
+}
+
+std::shared_ptr<RValue> Translator::E1()
+{
+	_currentLexem = _scanner.getNextToken();
+	/*n25*/
+	if (_currentLexem.type() == LexemType::num)
+	{
+		return std::make_shared<NumberOperand>(NumberOperand(_currentLexem.value()));
+	}
+	/*n26*/
+	if (_currentLexem.type() == LexemType::chr)
+	{
+		char tmp1 = (char)(_currentLexem.value());
+		std::string tmp2;
+		tmp2 += tmp1;
+		return _SymbolTable.add(tmp2);
+	}
+	syntaxError("Ошибка");
+	return nullptr;
 }
--- Translator.h
+++ Translator.h
@@ -15,9 +15,16 @@ public:
 	void printAtoms(std::ostream& stream);
 	void generateAtom(std::shared_ptr<Atom> atom);
 	std::shared_ptr<LabelOperand> newLabel();
+
+	void syntaxError(const std::string& message);
+	void lexicalError(const std::string& message);
+
+	std::shared_ptr<RValue> E1();
 protected:
 	std::vector<std::shared_ptr<Atom>> _atoms;
 	StringTable _StringTable = StringTable();
 	SymbolTable _SymbolTable = SymbolTable();
+	Token _currentLexem = Token(LexemType::eof);/*!!!*/
+	Scanner _scanner;/*!!!*/
 	int _lbl_counter = -1;
 };
